39158fe4f10bb83466b858044a4671596bc29104
mucileanustefan992-ui <mucileanustefan992@gmail.com>
2026-01-17 12:55:43 +0000

    wddwedwe

--- script.js
+++ script.js
@@ -21,13 +21,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     hiddenElements.forEach((el) => observer.observe(el));
-
-<script type="text/javascript">
-    (function(c,l,a,r,i,t,y)
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    )(window, document, "clarity", "script", "v2v4c4xjyd");
-</script>
-
 });
